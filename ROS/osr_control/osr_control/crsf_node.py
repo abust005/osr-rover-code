d9a9fe8d73507087aa547b06e5b1c7cc89b3a739
Adriel Bustamante <dpdbustamante@gmail.com>
2026-08-12 14:13:42 -0700

    Forgot close port

--- ROS/osr_control/osr_control/crsf_node.py
+++ ROS/osr_control/osr_control/crsf_node.py
@@ -48,7 +48,8 @@ class CRSF_RX(Node):
       return
 
     if not(self.crsf_port.is_paired()):
-      self.log.error("Transmitter not paired", throttle_duration_sec=5)
+      self.crsf_port.close_port()
+      self.log.error("Transmitter not paired, reinitializing", throttle_duration_sec=5)
       return
 
     self.log.info(f"Opened CRSF port on interface {self.interface}")
