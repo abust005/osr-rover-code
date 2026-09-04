18b0a818c8cd64ff9f947dab6a28b173d51e9e91
Adriel Bustamante <dpdbustamante@gmail.com>
2026-09-03 18:38:31 -0700

    Enabling mast control from transmitter

--- ROS/osr_control/osr_control/joy_extras.py
+++ ROS/osr_control/osr_control/joy_extras.py
@@ -5,7 +5,7 @@ from sensor_msgs.msg import Joy
 from rclpy.parameter import Parameter
 from rcl_interfaces.srv import SetParameters
 from rclpy.callback_groups import ReentrantCallbackGroup
-
+from osr_interfaces.msg import CommandMast
 
 class JoyButtonSubscriber(Node):
     """For custom functionality that the joy to twist publisher node can't handle"""
@@ -17,47 +17,61 @@ class JoyButtonSubscriber(Node):
         self.last_duty_mode_value = False
         self.last_received_time = None        
         self.cb_group = ReentrantCallbackGroup()
-        self.roboclaw_node_set_param_client = self.create_client(SetParameters, '/roboclaw_wrapper/set_parameters', 
-                                                                 callback_group=self.cb_group)
-        while not self.roboclaw_node_set_param_client.wait_for_service(timeout_sec=5.0):
-            self.log.info('Service /roboclaw_wrapper/set_parameters not available, waiting again...', skip_first=True)
+        # self.roboclaw_node_set_param_client = self.create_client(SetParameters, '/roboclaw_wrapper/set_parameters', 
+        #                                                          callback_group=self.cb_group)
+        # while not self.roboclaw_node_set_param_client.wait_for_service(timeout_sec=5.0):
+        #     self.log.info('Service /roboclaw_wrapper/set_parameters not available, waiting again...', skip_first=True)
         
         self.subscription = self.create_subscription(
             Joy,
-            'joy',
+            "/joy",
             self.joy_callback,
             10)
+
+        self.mast_pub = self.create_publisher(
+            CommandMast,
+            "/cmd_mast",
+            1
+        )
+
         self.log.info("Initialized JoyButtonSubscriber.")
 
-    async def joy_callback(self, msg):
+    def joy_callback(self, msg: Joy):
         if len(msg.buttons) <= self.duty_button_index:
             error_msg = f"button index {self.duty_button_index} exceeds the number of buttons available ({len(msg.buttons)})"
             self.log.error(error_msg)
             raise AttributeError(error_msg)
 
-        button_value = msg.buttons[self.duty_button_index]
-        current_time = self.get_clock().now()
-        # only flip the mode if we haven't received a button press in over a second
-        if button_value and (self.last_received_time is None or (current_time - self.last_received_time).nanoseconds > 1e9):
-            # change the duty mode to the opposite of what it was
-            param = Parameter(name="duty_mode", value=not self.last_duty_mode_value).to_parameter_msg()
-            self.log.info(f"Turning duty mode {'off' if self.last_duty_mode_value else 'on'}")
-            self.last_duty_mode_value = not self.last_duty_mode_value
-            await self.set_parameter_value_on_other_node(self.roboclaw_node_set_param_client, param)
-            self.last_received_time = current_time
+        # Construct CommandMast
+        mast_msg = CommandMast()
+        mast_msg.azimuth = msg.channels[10] * 1.57
+        mast_msg.elevation = msg.channels[11] * 1.57
 
-    async def set_parameter_value_on_other_node(self, node_set_param_client, param):
-        request = SetParameters.Request()
-        request.parameters.append(param)
-        future = node_set_param_client.call_async(request)
-        try:
-            result = await future
-        except Exception as e:
-            self.log.warning(f"Failed to call service with parameter {param.name}: {e}")
-        else:
-            res = result.results[0]
-            if not res.successful:
-                self.log.warning(f"Service call with parameter {param.name} unsuccessful: {res.reason}")
+        self.mast_pub.publish(msg)
+
+        # button_value = msg.buttons[self.duty_button_index]
+        # current_time = self.get_clock().now()
+        # # only flip the mode if we haven't received a button press in over a second
+        # if button_value and (self.last_received_time is None or (current_time - self.last_received_time).nanoseconds > 1e9):
+        #     # change the duty mode to the opposite of what it was
+        #     param = Parameter(name="duty_mode", value=not self.last_duty_mode_value).to_parameter_msg()
+        #     self.log.info(f"Turning duty mode {'off' if self.last_duty_mode_value else 'on'}")
+        #     self.last_duty_mode_value = not self.last_duty_mode_value
+        #     await self.set_parameter_value_on_other_node(self.roboclaw_node_set_param_client, param)
+        #     self.last_received_time = current_time
+
+    # async def set_parameter_value_on_other_node(self, node_set_param_client, param):
+    #     request = SetParameters.Request()
+    #     request.parameters.append(param)
+    #     future = node_set_param_client.call_async(request)
+    #     try:
+    #         result = await future
+    #     except Exception as e:
+    #         self.log.warning(f"Failed to call service with parameter {param.name}: {e}")
+    #     else:
+    #         res = result.results[0]
+    #         if not res.successful:
+    #             self.log.warning(f"Service call with parameter {param.name} unsuccessful: {res.reason}")
 
 def main(args=None):
     rclpy.init(args=args)
